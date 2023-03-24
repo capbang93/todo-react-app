03cba2f2f09e536cdff83ed8ece60565529c7207
Kim <capbang@sookmyung.ac.kr>
2023-03-24 16:51:59 +0900

    프론트개발 14. React+백엔드통합(24)

--- src/Todo.js
+++ src/Todo.js
@@ -3,39 +3,41 @@ import {ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, Ic
 import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
 
 class Todo extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={ item: props.item, readOnly:true };  // 매개변수 item 의 변수/값을 item에 대입
+        this.state = { item: props.item, readOnly: true };  // 매개변수 item 의 변수/값을 item에 대입
         this.delete = props.delete;
+        this.update = props.update;
     }
 
-    deleteEventHandler =()=>{
+    deleteEventHandler = () => {
         this.delete(this.state.item);
     }
-    offReadOnlyMode =()=>{
-        console.log("Event!",this.state.readOnly);
-        this.setState({readOnly:false},()=>{
-            console.log("ReadOnly?",this.state.readOnly)
+    offReadOnlyMode = () => {
+        this.setState({readOnly:false}, () =>{
+            console.log("ReadOnly?", this.state.readOnly)
         });
     }
-    enterKeyEventHandler =(e)=>{
-        if(e.key ==="Enter"){
-            this.setState({readOnly:true})
+    enterKeyEventHandler = (e) => {
+        if(e.key === "Enter") {
+            this.setState({readOnly:true});
+            this.update(this.state.item);
         }
     }
-    editEventHandler =(e)=>{
-        const thisItem =this.state.item;
+    editEventHandler = (e) => {
+        const thisItem = this.state.item;
         thisItem.title = e.target.value;
         this.setState({item:thisItem});
     }
-    checkboxEventHander =(e)=>{
-        console.log("check box event call");
-        const thisItem =this.state.item;
-        thisItem.done = thisItem.done ?false :true;// thisItemdone = !thisitem.done
-        this.setState({item:thisItem});
+    checkboxEventHander = (e) => {
+        const thisItem = this.state.item;
+        thisItem.done = thisItem.done ? false : true; // thisItemdone = !thisitem.done
+        //this.setState({item:thisItem});
+        this.setState({readOnly:true});
+        this.update(this.state.item);
     }
-    render(){
-        const item =this.state.item;
+    render() {
+        const item = this.state.item;
         return (
             <ListItem>
                 <Checkbox 
